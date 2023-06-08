91d1a24f629325999d47495f94684ef530dcbd0b
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2023-06-08 12:40:17 +0100

    Updating to carousel implementation

--- app/src/main/java/com/az/financeapp/ui/composables/NotificationCarousel.kt
+++ app/src/main/java/com/az/financeapp/ui/composables/NotificationCarousel.kt
@@ -1,30 +1,25 @@
 package com.az.financeapp.ui.composables
 
-import androidx.compose.animation.core.animate
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -33,15 +28,14 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_4
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import com.az.financeapp.R
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.VerticalPager
-import com.google.accompanist.pager.rememberPagerState
+import kotlin.math.absoluteValue
 
 data class NotificationUIModel(
     val text: String,
@@ -67,13 +61,37 @@ private val mockNotifications: List<NotificationUIModel> = listOf(
     )
 )
 
-@OptIn(ExperimentalPagerApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Preview(device = PIXEL_4)
 fun NotificationCarousel(notifications: List<NotificationUIModel> = mockNotifications) {
+    val pagerState = rememberPagerState()
 
-    VerticalPager(notifications.size) { page ->
-        CarouselItem(uiNotification = notifications[page])
+    VerticalPager(
+        modifier = Modifier.height(100.dp),
+        pageCount = notifications.size,
+        state = pagerState
+    ) { page ->
+
+        CarouselItem(modifier = Modifier
+            .graphicsLayer {
+                // Calculate the absolute offset for the current page from the
+                // scroll position. We use the absolute value which allows us to mirror
+                // any effects for both directions
+                val pageOffset = (
+                        (pagerState.currentPage - page) + pagerState
+                            .currentPageOffsetFraction
+                        ).absoluteValue
+
+                // We animate the alpha, between 50% and 100%
+                alpha = lerp(
+                    start = 0.5f,
+                    stop = 1f,
+                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                )
+            }, uiNotification = notifications[page]
+        )
+
     }
 
 }
@@ -81,16 +99,10 @@ fun NotificationCarousel(notifications: List<NotificationUIModel> = mockNotifica
 @Composable
 @Preview(device = PIXEL_4)
 private fun CarouselItem(
+    modifier: Modifier = Modifier,
     uiNotification: NotificationUIModel = mockNotifications[0]
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(9.dp)
-            .clip(RoundedCornerShape(20.dp))
-    )
-    {
+    Card(modifier = modifier) {
         Row(
             modifier = Modifier
                 .background(uiNotification.color)
