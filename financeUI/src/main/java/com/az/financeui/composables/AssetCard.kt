dd26c762df7d9de4e5534e7925ab7c411c26581b
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2023-06-30 19:14:40 +0100

    Finishing base of Asset Card

--- financeUI/src/main/java/com/az/financeui/composables/AssetCard.kt
+++ financeUI/src/main/java/com/az/financeui/composables/AssetCard.kt
@@ -1,16 +1,18 @@
 package com.az.financeui.composables
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -25,7 +27,9 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.az.financeui.R
-import com.az.financeui.theme.CryptoLigthGray
+import com.az.financeui.theme.CryptoWhite
+import com.az.financeui.theme.LightCarmin
+import com.az.financeui.theme.LightOlive
 
 data class AssetInfo(
     val iconDrawable: Int,
@@ -36,71 +40,107 @@ data class AssetInfo(
     val total: Float,
 )
 
+private val mockAssetInfo = AssetInfo(
+    R.drawable.apple,
+    "Apple Inc.",
+    "AAPL",
+    listOf(
+        180.06f,
+        181.82f,
+        182.39f,
+        183.83f,
+        184.97f,
+        185.02f,
+        186.07f,
+        187.03f,
+        188.06f,
+        189.82f,
+        190.39f,
+        191.83f,
+        192.97f,
+        193.02f,
+        194.07f,
+        195.03f,
+        194.07f
+    ),
+    187.00023f,
+    1870.3f
+)
+
 @Composable
 @Preview(device = Devices.PIXEL_4_XL)
 fun AssetCard(
-    assetInfo: AssetInfo = AssetInfo(
-        R.drawable.apple,
-        "Apple Inc.",
-        "AAPL",
-        listOf(186.06f, 189.82f, 189.39f, 189.83f, 189.97f, 185.02f, 186.07f, 187.03f),
-        187.03f,
-        1870.3f
-    )
+    assetInfo: AssetInfo = mockAssetInfo
 ) {
     Card(
         modifier = Modifier
             .wrapContentHeight()
             .fillMaxWidth()
-            .background(Color.White)
-            .padding(5.dp)
+            .padding(5.dp),
+        colors = CardDefaults.cardColors(containerColor = CryptoWhite)
     ) {
         Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp).fillMaxWidth()
         ) {
             AssetIcon(assetInfo.iconDrawable)
 
             TickerName(assetInfo.name, assetInfo.tickerName)
 
             PerformanceChart(assetInfo.lastDayChange)
+
+            ValueView(assetInfo.currentValue, assetInfo.total)
         }
     }
 }
 
 @Composable
-@Preview
+fun ValueView(currentValue: Float, total: Float) {
+    Column(
+        modifier = Modifier
+            .padding(start = 10.dp),
+        horizontalAlignment = Alignment.End
+    ) {
+        Text(
+            text = currentValue.toString(),
+            style = MaterialTheme.typography.labelMedium,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Text(
+            text = "$${total.toInt()}",
+            style = MaterialTheme.typography.labelSmall,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+//@Preview
 fun PerformanceChart(
-    lastDayChange: List<Float> = listOf(
-        186.06f,
-        189.82f,
-        189.39f,
-        189.83f,
-        189.97f,
-        185.02f,
-        186.07f,
-        187.03f
-    )
+    lastDayChange: List<Float> = mockAssetInfo.lastDayChange
 ) {
 
     Box(contentAlignment = Alignment.Center) {
         val pairsList = lastDayChange.zipWithNext()
-        val offsetIncrement = 10f
+        val offsetIncrement = 24f
 
         Canvas(modifier = Modifier
-            .size(40.dp)
+            .width(150.dp)
+            .height(70.dp)
             .padding(5.dp), onDraw = {
 
-            var lastLineEnd = Offset(0f, 0f)
+            var lastLineEnd = Offset(0f, 170f)
             var pointDiff: Float
             val lineColor =
-                if (lastDayChange.last() > lastDayChange.first()) Color.Green else Color.Red
+                if (lastDayChange.last() > lastDayChange.first()) LightOlive else LightCarmin
+            val yModifier = -10f
 
             for (pair in pairsList) {
                 val start = pair.first
                 val end = pair.second
-                pointDiff = end - start
+                pointDiff = (end - start) * yModifier
 
                 val newLineEnd = Offset(lastLineEnd.x + offsetIncrement, lastLineEnd.y + pointDiff)
 
@@ -108,7 +148,7 @@ fun PerformanceChart(
                     color = lineColor,
                     start = lastLineEnd,
                     end = newLineEnd,
-                    strokeWidth = 2.0f
+                    strokeWidth = 4.0f
                 )
                 lastLineEnd = newLineEnd
             }
@@ -122,7 +162,6 @@ fun PerformanceChart(
 private fun TickerName(name: String = "Apple Inc.", tickerName: String = "AAPL") {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(start = 10.dp)
     ) {
         Text(
@@ -138,11 +177,11 @@ private fun TickerName(name: String = "Apple Inc.", tickerName: String = "AAPL")
 @Composable
 //@Preview
 private fun AssetIcon(iconDrawable: Int = R.drawable.apple) {
-    Box(modifier = Modifier.size(40.dp), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier.size(50.dp), contentAlignment = Alignment.Center) {
         Canvas(modifier = Modifier, onDraw = {
-            val radius = 50f
+            val radius = 65f
             drawCircle(
-                color = CryptoLigthGray,
+                color = Color.White,
                 radius = radius
             )
         })
